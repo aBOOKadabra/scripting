5d4659fe1d3ff927c41fdbc632045e2028d9a1f0
Raphael Marbeck <rmarbeck@gmail.com>
2013-11-04 17:44:03 +0100

    Adding parsing scripts for generating yml init files.

---
+++ linux/parsing/parse_csv_to_yaml.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+
+#=====================================================================
+# _ubeg_
+#
+# USAGE :
+#   %facility% command
+#
+# DESCRIPTION :
+#   Parsing a csv file containing authors to yml equivalent via awk
+#
+# OPTIONS :
+#       source file
+#       type of file to parse [author, category]
+#       output file
+#
+# RETURN CODES :
+#   <Return code> <meaning>
+#   0 everything was allright
+#   1 bad options
+#   2 unknown issue
+#
+# WARNINGS :
+#   None
+#
+# EXAMPLES :
+#   %facility%
+#
+# _uend_
+#=====================================================================
+
+current_dir=`pwd`
+
+Facility=$0
+facility=`basename ${Facility}`
+facility_dir=`dirname ${Facility}`
+
+# Loading conf
+. ${Facility%%.sh}.conf
+
+exec 2>>${error_file}
+
+#============= DISPLAYING FUNCTIONS ==================================
+function display_step {
+    echo -e "$1 ........... \c"
+}
+
+function display_step_status {
+    if [ $1 = 0 ] ; then
+        display_OK
+        return 0
+    else
+        display_KO
+        return 1
+    fi	
+}
+
+function display_OK {
+    echo -e '[\c'
+#    tput setaf 2
+    echo -e "OK\c"
+#    tput setaf 0
+    echo -e ']'
+}
+
+function display_KO {
+    echo -e '[\c'
+#    tput setaf 1
+    echo -e "KO\c"
+#    tput setaf 0
+    echo -e ']'
+}
+#=====================================================================
+
+#============= ERROR HANDLING FUNCTIONS ==============================
+function halt_on_error {
+    display_KO
+    if [[ "$send_mail_on_error" -eq "yes" ]]
+    then
+        send_mail_alert "$2" $warning_email
+    fi
+    exit $1
+}
+
+function send_mail_alert {
+#    mail -s "$facility failed" $2 << eof
+    `date`
+    $facility Failed : $1
+eof
+}
+#=====================================================================
+
+#============= COMMON FUNCTIONS ======================================
+# Description of the script
+function usage {
+    echo "Error in options"
+    sed "/^# _ubeg_/,/^# _uend_/!d;/_ubeg_/d;/_uend_/d;s/^#//g;s/%faci[^%]*%/$facility/g" $Facility
+}
+
+# Logging to STDOUT or log file
+function log {
+    echo `date '+%d/%m/%Y %H:%M:%S'` - $1 | tee -a ${log_file}
+}
+
+function function_exists {
+    FUNCTION_NAME=$1
+
+    [ -z "$FUNCTION_NAME" ] && return 1
+
+    declare -f -F "$FUNCTION_NAME" > /dev/null 2>&1
+
+    return_code=$?
+
+    return $return_code
+}
+#=====================================================================
+# Initialising
+#  - Checking if the same script is running (checking if temp_dir exists)
+#  - Creating dirs
+function init {
+    display_step initing
+    echo `date '+%d/%m/%Y %H:%M:%S'` - Starting >> ${error_file}
+    if [[ -d ${temp_dir} ]]
+    then
+        message="${temp_dir} exists, the script is already running or have previously failed"
+        echo $message | tee -a ${error_file}
+        halt_on_error 2 "$message"
+    fi
+    mkdir -p ${temp_dir}
+    display_OK
+    log "Starting : $@"
+}
+
+#  Ending :
+#  - Removing temp dir
+function uninit {
+    display_step uniniting
+    cd ${root_dir};rm -rf ${temp_dir}
+    display_OK
+    echo `date '+%d/%m/%Y %H:%M:%S'` - End >> ${error_file}
+}
+
+# Stopping on erro
+function is_ok {
+    return_code=$1
+    if [[ $return_code -ne 0 ]]
+    then
+        halt_on_error 3 "$2 failed with return code $return_code"
+    fi
+    display_OK
+}
+
+#============= MAIN FUNCTIONS ======================================
+# Create a file for each mail already there before doing anything
+function parse_author {
+cat $file_to_parse | $awk_bin 'BEGIN { print "authors:";
+              FS=";"}
+      $1 ~ /Head 1/ {  split($0, cat, ";") }
+      $1 ~ /Auteur/ { split ($1, author, " ");print "    - &"author[1]author[2]" !!models.Author" }
+      $1 ~ /Auteur/ { print "         fullname:"}
+      $1 ~ /Auteur/ { split ($2, fullname, " "); print "               name:          "fullname[2]" "fullname[3]"\n               firstname:     "fullname[1]}
+      $1 ~ /Auteur/ { print "         score:"}
+      $1 ~ /Auteur/ { print "               bestseller:    "$34}
+      $1 ~ /Auteur/ { print "               classical:     "$35}
+      $1 ~ /Auteur/ { print "               ico:           "$36}
+      $1 ~ /Auteur/ { print "               hard:          "$37}
+      $1 ~ /Auteur/ { split ($1, author, " ");print "               author:        *"author[1]author[2]}
+      $1 ~ /Auteur/ { print "         categories:"}
+      $1 ~ /Auteur/ { for (i=4;i<=33;i++) {
+                            if ($i != "") {
+                                {print "            - {category: *"cat[i]", percentage: "$i" }" }
+                            }
+                        }
+                      }
+      $1 ~ /Auteur/ { print ""}
+      END   { print "#Fin" }
+' > $output_file
+
+}
+
+function main {
+    function_exists parse_$action
+    return_code=$?
+
+    if [[ $? -eq 0 ]]; then 
+        parse_$action
+    else
+        usage
+    fi
+}
+
+#=====================================================================
+#---------------------------------------------------------------------
+# Reading options
+#---------------------------------------------------------------------
+args="$@"
+
+if [ $# -eq 0 ]||[ $# -gt 3 ]; then
+    usage
+    display_step "reading arguments"
+    halt_on_error 1 "Too many args : $args"
+fi
+
+
+if [[ $# -ge 1 ]]; then
+    file_to_parse=$1
+    action=author
+    output_file=$1_result.yml
+fi
+if [[ $# -ge 2 ]]; then
+    action=$2
+    output_file=$1_result.yml
+fi
+if [[ $# -eq 3 ]]; then
+    output_file=$3
+fi
+
+init "$args"
+main
+uninit
+log "The End" 
+
